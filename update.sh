b6b3f658dcb70bf1718585da079e1eeb5bd1fbb4
AminduKaushal <amixxdu@gmail.com>
2024-01-17 20:40:52 +0530

    docs : updated readme

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@
 sudo rm -rf ./configs
 
 # Copy the contents from the Docker container to the 'configs' folder
-sudo docker cp 11e06373e89f:/home/wso2carbon/wso2am-4.0.0/repository/ ./configs
+sudo docker cp container_id:/home/wso2carbon/wso2am-4.0.0/repository/ ./configs
